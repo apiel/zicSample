41bba6ea5aaaef30bbd59b0177e20e7a45cddbbf
Alexandre Piel <Alexandre.Piel@zooplus.com>
2023-04-27 16:08:32 +0200

    use grid in freesound

--- viewFreesound.h
+++ viewFreesound.h
@@ -9,8 +9,7 @@
 class ViewFreesound : public View {
 protected:
     Freesound& data = Freesound::get();
-
-    int8_t cursorPos = 0;
+    Grid grid = Grid(4, 2);
     uint8_t currentPos = 0;
 
     static ViewFreesound* instance;
@@ -26,49 +25,50 @@ protected:
 
     void renderItem(unsigned int pos)
     {
-        unsigned int marginLeft = 25;
-
         unsigned int y = 35 + pos * (h + 5);
         drawFilledRect({ 5, y }, { SCREEN_W - 10, h }, COLOR_FOREGROUND);
-        y += marginTop;
-        drawText({ 10 + marginLeft, y + 2 }, data.items[currentPos + pos].name, COLOR_INFO_LIGHT);
-        drawText({ 10 + marginLeft, y + 22 }, data.items[currentPos + pos].tags, COLOR_INFO_DARK, 12);
 
-        char info[20];
-        unsigned int x = drawText({ 10 + marginLeft, y + 39 }, "Size: ", COLOR_INFO_DARK, 12);
-        x = drawText({ x + 2, y + 39 }, data.items[currentPos + pos].filesizeStr, COLOR_INFO, 12);
-        x = drawText({ x + 10, y + 39 }, "Duration: ", COLOR_INFO_DARK, 12);
-        sprintf(info, "%.1fsec", data.items[currentPos + pos].duration);
-        x = drawText({ x + 2, y + 39 }, info, COLOR_INFO, 12);
-        x = drawText({ x + 10, y + 39 }, "Downloads: ", COLOR_INFO_DARK, 12);
-        sprintf(info, "%d", data.items[currentPos + pos].num_downloads);
-        x = drawText({ x + 2, y + 39 }, info, COLOR_INFO, 12);
-        x = drawText({ x + 10, y + 39 }, "Rating: ", COLOR_INFO_DARK, 12);
-        sprintf(info, "%.1f/5", data.items[currentPos + pos].avg_rating);
-        drawText({ x + 2, y + 39 }, info, COLOR_INFO, 12);
-
+        y += 5;
         drawFilledRect({ 7, y + 2 }, { 20, 20 }, COLOR_INFO_DARK);
         drawPlayButton({ 12, y + 7 }, { 10, 10 }, COLOR_ON);
 
-        drawFilledRect({ 7, y + 30 }, { 20, 20 }, COLOR_INFO_DARK);
-        drawDownloadButton({ 10, y + 31 }, { 13, 15 }, COLOR_INFO);
+        drawFilledRect({ 35, y + 2 }, { 20, 20 }, COLOR_INFO_DARK);
+        drawDownloadButton({ 38, y + 3 }, { 13, 15 }, COLOR_INFO);
 
-        renderSelection();
+        drawText({ 65, y + 2 }, data.items[currentPos + pos].name, COLOR_INFO_LIGHT);
+
+        y += 30;
+        drawText({ 10, y }, data.items[currentPos + pos].tags, COLOR_INFO_DARK, 12);
+
+        y += 15;
+        char info[20];
+        unsigned int x = drawText({ 10, y }, "Size: ", COLOR_INFO_DARK, 12);
+        x = drawText({ x + 2, y }, data.items[currentPos + pos].filesizeStr, COLOR_INFO, 12);
+        x = drawText({ x + 10, y }, "Duration: ", COLOR_INFO_DARK, 12);
+        sprintf(info, "%.1fsec", data.items[currentPos + pos].duration);
+        x = drawText({ x + 2, y }, info, COLOR_INFO, 12);
+        x = drawText({ x + 10, y }, "Downloads: ", COLOR_INFO_DARK, 12);
+        sprintf(info, "%d", data.items[currentPos + pos].num_downloads);
+        x = drawText({ x + 2, y }, info, COLOR_INFO, 12);
+        x = drawText({ x + 10, y }, "Rating: ", COLOR_INFO_DARK, 12);
+        sprintf(info, "%.1f/5", data.items[currentPos + pos].avg_rating);
+        drawText({ x + 2, y }, info, COLOR_INFO, 12);
+
+        renderSelection(grid.row, grid.col);
     }
 
-    void renderSelection(bool clear = false)
+    void renderSelection(int8_t row, int8_t col, bool clear = false)
     {
         SDL_Color color = COLOR_INFO_LIGHT;
         if (clear) {
             color = COLOR_FOREGROUND;
         }
-        unsigned int pos = cursorPos * 0.5;
-        unsigned int y = marginTop + 35 + pos * (h + 5);
+        unsigned int y = marginTop + 35 + row * (h + 5);
 
-        if (cursorPos % 2 == 0) {
+        if (col == 0) {
             drawRect({ 7, y + 2 }, { 20, 20 }, color);
         } else {
-            drawRect({ 7, y + 30 }, { 20, 20 }, color);
+            drawRect({ 35, y + 2 }, { 20, 20 }, color);
         }
     }
 
@@ -103,23 +103,17 @@ public:
         if (keys.Menu) {
             ui.view = VIEW_MAIN;
             ui.needMainViewRender = true;
-        } else if (keys.Up) {
-            if (cursorPos > 0) {
-                renderSelection(CLEAR);
-                cursorPos--;
-                renderSelection();
+        } else if (grid.update(keys) == VIEW_CHANGED) {
+            if (grid.rowChanged() || grid.colChanged()) {
+                renderSelection(grid.lastRow, grid.lastCol, CLEAR);
+                renderSelection(grid.row, grid.col);
                 draw();
-            } else if (currentPos > 0) {
-                currentPos--;
-                render();
-                draw();
-            }
-        } else if (keys.Down) {
-            if (cursorPos < 7) {
-                renderSelection(CLEAR);
-                cursorPos++;
-                renderSelection();
-                draw();
+            } else if (grid.row == 0) {
+                if (currentPos > 0) {
+                    currentPos--;
+                    render();
+                    draw();
+                }
             } else if (currentPos + 8 < data.getCount()) {
                 currentPos++;
                 render();
