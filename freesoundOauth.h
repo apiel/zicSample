d542f7ca87364f26edbaa48394d82a3c4b1430c1
Alexandre Piel <Alexandre.Piel@zooplus.com>
2023-04-29 22:32:35 +0200

    wip

--- freesoundOauth.h
+++ freesoundOauth.h
@@ -5,14 +5,9 @@
 
 static size_t freesoundOauthHeaderCallback(char* buffer, size_t size, size_t nitems, void* userdata)
 {
-    // APP_LOG("freesoundCookieCallback: %s\n", buffer);
+    // APP_LOG("freesoundOauthHeaderCallback: %s\n", buffer);
     char * cookie = strstr(buffer, "Set-Cookie:");
     if (cookie) {
-        // cookie += 12; // strlen("Set-Cookie:")
-        // // char* end = strstr(cookie, ";");
-        // // if (end) {
-        // //     *end = '\0';
-        // // }
         strcpy((char *)userdata, cookie);
     }
     return nitems * size;
@@ -80,13 +75,16 @@ protected:
             // curl_easy_setopt(curl, CURLOPT_URL, "https://freesound.org/apiv2/login/");
             curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
             curl_easy_setopt(curl, CURLOPT_POSTFIELDS, postFields);
-            // curl_easy_setopt(curl, CURLOPT_COOKIE, cookies);
             struct curl_slist* headerlist = NULL;
             headerlist = curl_slist_append(headerlist, cookies);
+            headerlist = curl_slist_append(headerlist, "Origin: https://freesound.org");
+            headerlist = curl_slist_append(headerlist, "Host: freesound.org");
+
             char referer[512];
             sprintf(referer, "Referer: %s", url);
             APP_LOG("++++++ set referer: %s\n", referer);
             headerlist = curl_slist_append(headerlist, referer);
+
             curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headerlist);
             CURLcode res = curl_easy_perform(curl);
             if (res != CURLE_OK) {
---
+++ oauth/main.js
@@ -0,0 +1,64 @@
+async function main() {
+    const username = process.argv[2];
+    const password = process.argv[3];
+
+    const res = await fetch(
+        'https://freesound.org/apiv2/oauth2/authorize/?client_id=aOE7UaYW68l205T8RHuH&response_type=code'
+    );
+    const text = await res.text();
+
+    var inputs = text.match(/<input.+?\/?>/g);
+    console.log(inputs);
+    const csrfSearch = 'name="csrfmiddlewaretoken" value="';
+    const csrfIndex = text.indexOf(csrfSearch);
+    const csrf = text.substring(
+        csrfIndex + csrfSearch.length,
+        text.substring(csrfIndex + csrfSearch.length).indexOf('"') + csrfIndex + csrfSearch.length
+    );
+
+    const nextSearch = 'name="next" value="';
+    const nextIndex = text.indexOf(nextSearch);
+    const next = text.substring(
+        nextIndex + nextSearch.length,
+        text.substring(nextIndex + nextSearch.length).indexOf('"') + nextIndex + nextSearch.length
+    );
+
+    const [cookie] = res.headers.get('Set-Cookie').split(';');
+    console.log({
+        next,
+        csrf,
+        username,
+        password,
+        referer: res.url,
+        setCookie: res.headers.get('Set-Cookie'),
+        cookie,
+    });
+
+    const data = new URLSearchParams();
+    data.append('csrfmiddlewaretoken', csrf);
+    data.append('next', next);
+    data.append('username', username);
+    data.append('password', password);
+    const res2 = await fetch(
+        'https://freesound.org/apiv2/oauth2/authorize/?client_id=aOE7UaYW68l205T8RHuH&response_type=code',
+        {
+            method: 'POST',
+            body: data,
+            // referrer: res.url,
+            // origin: 'https://freesound.org',
+            // coookies: res.headers.get('Set-Cookie'),
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                Referer: res.url,
+                // 'Set-Cookie': res.headers.get('Set-Cookie'),
+                // 'Cookie': res.headers.get('Set-Cookie'),
+                Cookie: cookie,
+                Origin: 'https://freesound.org',
+            },
+        }
+    );
+    const text2 = await res2.text();
+    console.log(text2);
+}
+
+main();
